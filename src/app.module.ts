7c31cd7f945f73f72ff5934d41102ebbc1b0bd23
Matheus da Silva <contagodzillajapan@gmail.com>
2024-11-10 00:03:20 -0300

    Feat: removendo configuração do Prettier e adicionando módulo Produtos ao AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProdutosService } from './produtos/produtos.service';
+import { ProdutosController } from './produtos/produtos.controller';
+import { ProdutosModule } from './produtos/produtos.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ProdutosModule],
+  controllers: [AppController, ProdutosController],
+  providers: [AppService, ProdutosService],
 })
 export class AppModule {}
