2256104e98ba8b326c200f72f02332d640b1cbcc
Matheus da Silva <contagodzillajapan@gmail.com>
2024-11-10 00:02:56 -0300

    Feat: adicionando serviços e controladores ao módulo Produtos

--- src/produtos/produtos.module.ts
+++ src/produtos/produtos.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ProdutosService } from './produtos.service';
+import { ProdutosController } from './produtos.controller';
 
-@Module({})
+@Module({
+  providers: [ProdutosService],
+  controllers: [ProdutosController]
+})
 export class ProdutosModule {}
