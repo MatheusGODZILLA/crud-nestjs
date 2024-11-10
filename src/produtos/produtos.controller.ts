2cd5c09e7440761b1c34dd692b4d9c170db8d863
Matheus da Silva <contagodzillajapan@gmail.com>
2024-11-10 00:00:44 -0300

    Feat: implementando injeção de dependência e os métodos CRUD

--- src/produtos/produtos.controller.ts
+++ src/produtos/produtos.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Patch, Delete, Body, Param, ParseIntPipe } from '@nestjs/common';
+import { Produto } from '../interfaces/produto.interface';
+import { ProdutosService } from './produtos.service';
 
 @Controller('produtos')
-export class ProdutosController {}
+export class ProdutosController {
+    constructor(private produtoService: ProdutosService) {}
+
+    @Get()
+    findAll(): Produto[] {
+        return this.produtoService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id', ParseIntPipe) id: number): Produto {
+        return this.produtoService.findOne(id);
+    }
+
+    @Post()
+    create(@Body() produto: Produto): Produto {
+        return this.produtoService.create(produto);
+    }
+
+    @Patch(':id')
+    update(@Param('id', ParseIntPipe) id: number, @Body() produto: Produto): Produto {
+        return this.produtoService.update(id, produto);
+    }
+
+    @Delete(':id')
+    delete(@Param('id', ParseIntPipe) id: number): void {
+        this.produtoService.delete(id);
+    }
+}
