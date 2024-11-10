9f22aa94e308b9957db66617f49b4348a65b5271
Matheus da Silva <contagodzillajapan@gmail.com>
2024-11-09 23:55:35 -0300

    Feat: definindo atributos e construção do produdo

--- src/produtos/produtos.service.ts
+++ src/produtos/produtos.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { Produto } from '../interfaces/produto.interface';
 
 @Injectable()
-export class ProdutosService {}
+export class ProdutosService {
+    private produtos: Produto[] = [];
+    private id: number = 1;
+
+    create(produto: Produto): Produto {
+        produto.id = this.id++;
+        this.produtos.push(produto);
+        return produto;
+    }
+}
